76b90f0f7b462076643827cb183f909ee7f22cf0
Angela <michellevidal16a@gmail.com>
2025-04-11 20:17:24 -0300

    new

--- src/game/main/Game.java
+++ src/game/main/Game.java
@@ -1,40 +1,87 @@
-
 package game.main;
-
-import static jdk.internal.misc.OSEnvironment.initialize;
 
 public class Game implements Runnable {
 
-    //constantes
+    // Constantes
+    private static final int MILLIS_PER_SEC = 1000;
+    private static final int NANOS_PER_SEC = 1000000000;
+    private static final double NUM_TICKS = 60.0;
 
-    //variaveis
+    // Variáveis
     private boolean running;
 
-    //componentes
+    // Componentes
+    private Thread thread;
 
-    public Game(){
+    public Game() {
         initialize();
-
     }
+
     public static void main(String[] args) {
         new Game();
-
     }
 
-    private void initialize{
+    private void initialize() {
         start();
     }
 
-    private synchronized void start(){
-           running = true;
+    private synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
     }
 
-    private synchronized void stop(){
-          running = false;
+    private synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = NUM_TICKS;
+        double ns = NANOS_PER_SEC / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        int updates = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while (delta >= 1) {
+                tick();
+                updates++;
+                delta--;
+            }
+
+            if (running) {
+                render();
+                frames++;
+            }
+
+            if (System.currentTimeMillis() - timer > MILLIS_PER_SEC) {
+                timer += MILLIS_PER_SEC;
+                System.out.println("FPS: " + frames + " | TICKS: " + updates);
+                updates = 0;
+                frames = 0;
+            }
+        }
+
+        stop();
+    }
+
+    private void tick() {
+
+    }
+
+    private void render() {
 
     }
 }
